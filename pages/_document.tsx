1ad0ecc30512733de1c51c6f086b5c4953623d68
Oosasukel <rodrigo_gonn@hotmail.com>
2021-08-31 01:06:21 -0300

    first commit

---
+++ pages/_document.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { IconContext } from 'react-icons'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
+        <Html lang='en'>
+          <Head>
+            <link
+              rel='apple-touch-icon'
+              sizes='76x76'
+              href='/apple-touch-icon.png'
+            />
+            <link
+              rel='icon'
+              type='image/png'
+              sizes='32x32'
+              href='/favicon-32x32.png'
+            />
+            <link
+              rel='icon'
+              type='image/png'
+              sizes='16x16'
+              href='/favicon-16x16.png'
+            />
+            <link rel='manifest' href='/site.webmanifest' />
+            <link
+              rel='mask-icon'
+              href='/safari-pinned-tab.svg'
+              color='#eb2929'
+            />
+            <meta name='msapplication-TileColor' content='#da532c' />
+            <meta name='theme-color' content='#ffffff'></meta>
+          </Head>
+
+          <body>
+            <script src='noflash.js' />
+
+            <Main />
+
+            <NextScript />
+          </body>
+        </Html>
+      </IconContext.Provider>
+    )
+  }
+}
---
+++ site.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  // where it all starts -- the site's root Notion page (required)
+  rootNotionPageId: '91919f9bd4e64a628dc439bb23fd648b',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: 'Rodrigo Gonçalves',
+  domain: 'rodrigog.com.br',
+  author: 'Rodrigo Gonçalves',
+
+  // open graph metadata (optional)
+  description: 'Blog sobre games e TI',
+  socialImageTitle: 'Blog',
+  socialImageSubtitle: 'Blog sobre games e TI',
+
+  // social usernames (optional)
+  twitter: '',
+  github: 'Oosasukel',
+  linkedin: 'rodrigogon',
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // image CDN host to proxy all image requests through (optional)
+  // NOTE: this requires you to set up an external image proxy
+  imageCDNHost: null,
+
+  // Utteranc.es comments via GitHub issue comments (optional)
+  utterancesGitHubRepo: null,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  // NOTE: this requires you to set up Google Firebase and add the environment
+  // variables specified in .env.example
+  isPreviewImageSupportEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null
+}
